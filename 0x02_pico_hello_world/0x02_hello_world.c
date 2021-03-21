623e0b9181d5d4f0c4027673895c9d7eaa95cbcc
Kevin Thomas <kevin@mytechnotalent.com>
2021-03-21 05:31:31 -0400

    Added files for 0x02_hello_world

---
+++ 0x02_pico_hello_world/0x02_hello_world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+int main() 
+{
+  stdio_init_all();
+
+  while(1) 
+  {
+    printf("Hello world!\n");
+    sleep_ms(1000);
+  }
+    
+  return 0;
+}
