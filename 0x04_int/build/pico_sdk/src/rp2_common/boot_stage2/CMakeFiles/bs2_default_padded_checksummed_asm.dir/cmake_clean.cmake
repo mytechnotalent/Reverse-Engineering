12682b0909de8465e9808e9216a05f0e64c17402
Kevin Thomas <kevin@mytechnotalent.com>
2021-03-31 06:03:46 -0400

    Added files for 0x04_int

--- 0x04_int/build/pico_sdk/src/rp2_common/boot_stage2/CMakeFiles/bs2_default_padded_checksummed_asm.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bs2_default_padded_checksummed_asm"
-  "bs2_default_padded_checksummed.S"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bs2_default_padded_checksummed_asm.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
