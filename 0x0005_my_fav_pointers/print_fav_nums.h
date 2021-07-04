d0158e930c703d7403e26d5abac04412116f21d7
Kevin Thomas <kevin@mytechnotalent.com>
2021-07-04 13:45:08 -0400

    Added files for 0x0005_pointers

---
+++ 0x0005_my_fav_pointers/print_fav_nums.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/**
+ * Function proc to print fav_nums
+ * @param fav_nums   Favorite numbers
+ */
+void print_fav_nums_proc(const std::vector<int> *const fav_nums);
---
+++ 0x0005_pointers/build_arr.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * Function to build an array
+ * @param size          Size of array
+ * @param init_value    Initial value of array
+ * @returns arr         Built array
+ */
+int *build_arr(size_t size, int init_value = 0);
