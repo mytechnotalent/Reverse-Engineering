d718ca2e12ff9dfd8d7c2b7d2ad382cdc65b93f9
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-10 20:19:09 -0500

    Delete 0x0004_functions directory

--- 0x0004_functions/print_fav_nums_by_value.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/**
- * Function proc to take in a favorite number by value, print/manipulate it
- * @param favorite_number   Value of the favorite number
- */ 
-void print_fav_nums_by_value_proc(int favorite_number);
--- 0x0004_functions/sum.cpp
+++
@@ -1,6 +0,0 @@
-#include "sum.h"
-
-int sum(int favorite_number, int unfavorite_number) 
-{
-    return favorite_number + unfavorite_number;
-}
