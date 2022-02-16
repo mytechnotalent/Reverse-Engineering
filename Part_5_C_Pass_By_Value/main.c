8f39742807a80033a26b70cf4fb4da887bebb23d
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-16 11:21:58 -0500

    Added files for Chapter 6 fg

--- Part_5_C_Pass_By_Value/main.c
+++ Part_5_C_Pass_By_Value/main.c
@@ -13,9 +13,13 @@ int main(void)
     int b = 100;
 
     printf("a is %d, b is %d: BEFORE SWAP\n", a, b);
+    printf("address of a: %p\n", &a);
+    printf("address of b: %p\n", &b);
 
     swap(a, b);
     printf("a is %d, b is %d: AFTER SWAP\n", a, b);
+    printf("address of a: %p\n", &a);
+    printf("address of b: %p\n", &b);
 
     return 0;
 }
@@ -27,4 +31,6 @@ void swap(int c, int d)
     c = d;
     d = temp;
     printf("c is %d, d is %d: INSIDE SWAP\n", c, d);
+    printf("address of c: %p\n", &c);
+    printf("address of d: %p\n", &d);
 }
